8da4c1002c31b148a275762a3c85a6f26f3cda9f
jennyunchan <jenniferyunchan@gmail.com>
2020-03-01 00:01:52 +0000

    Updates

---
+++ precache-manifest.5e46d167eb45a7fb5c551b6e75fe5327.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6e9df18f7426e3c93e9610b46f384d",
+    "url": "/expense-management-app/index.html"
+  },
+  {
+    "revision": "53496d5adc6806a9483d",
+    "url": "/expense-management-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "74f16bc4129b51cc5a2f",
+    "url": "/expense-management-app/static/js/2.24253b02.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/expense-management-app/static/js/2.24253b02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53496d5adc6806a9483d",
+    "url": "/expense-management-app/static/js/main.8b3b24ff.chunk.js"
+  },
+  {
+    "revision": "d9d4b5325cf46a23aaee",
+    "url": "/expense-management-app/static/js/runtime-main.833b8afd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/expense-management-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
