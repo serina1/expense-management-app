3eb8318f7ca800669fc5b7f0e3aae66d84342d44
jennyunchan <jenniferyunchan@gmail.com>
2020-03-09 16:16:59 +0000

    added methods to user schema to hash password upon account creation and to check if it is valid

--- models/userModel.js
+++ models/userModel.js
@@ -18,6 +18,14 @@ const UserSchema = new Schema({
   }
 });
 
+UserSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8). null);
+}
+
+UserSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+}
+
 const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
