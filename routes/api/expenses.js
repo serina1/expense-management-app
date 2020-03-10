cb2098db08a98d18bbdc50cc914a992e86b07d21
Serina Mohammed <serina_4695@hotmail.co.uk>
2020-03-10 20:13:12 +0000

    testing connection to front end

--- routes/api/expenses.js
+++ routes/api/expenses.js
@@ -12,19 +12,61 @@ router.route("/").get((req, res) => {
   
   router.route("/add").post((req, res) => {
     const title = req.body.title;
-    const date = req.body.date;
+    const date = Date.parse(req.body.date);
     const category = req.body.category;
     const clienttocharge = req.body.clienttocharge;
-    const amount = req.body.amount;
+    const amount = Number(req.body.amount);
     const notes = req.body.notes; 
   
-    const newExpense = new Expense({ title,date, category, clienttocharge, amount, notes});
+    const newExpense = new Expense({ 
+      title,
+      date,
+      category,
+      clienttocharge, 
+      amount, 
+      notes
+    });
   
     newExpense
       .save()
       .then(() => res.json("Expense added!"))
       .catch(err => res.status(400).json("Error: " + err));
   });
+
+  router.route('/:id').get((req, res) => {
+    console.log(req.params)
+    Expense.findById(req.params.id)
+    .then(expense => res.json(expense))
+    .catch(err => res.status(400).json("Error:" + err))
+  });
+
+  router.route('/:id').delete ((req, res) => {
+    Expense.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Expense Deleted"))
+    .catch(err => res.status(400).json("Error:" + err))
+  });
   
+  router.route('/update/:id').post ((req, res) => {
+    Expense.findById(req.params.id)
+    .then(expense => {
+
+      expense.title = req.body.title,
+      expense.date = Date.parse(req.body.date);
+      expense.category = req.body.category;
+      expense.clienttocharge = req.body.clienttocharge;
+      expense.amount = Number (req.body.amount);
+      expense.notes = req.body.notes
+    
+
+      expense.save()
+     .then(() => res.json("Expense updated")) 
+     .catch(err => res.status(400).json("Error:" + err));
+    
+    })
+    
+    .catch(err => res.status(400).json("Error:" + err));
+
+      
+  });
 
 module.exports = router;
